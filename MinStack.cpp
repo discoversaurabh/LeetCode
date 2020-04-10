e12dc78d61f482b28a429361ef4069ee20d82238
discoversaurabh <discover.saurabh@gmail.com>
2020-04-11 03:14:57 +0530

    Design a stack(LIFO) that retrieves minimum element in constant time
    https://leetcode.com/explore/featured/card/30-day-leetcoding-challenge/529/week-2/3292/

---
+++ MinStack.cpp
@@ -0,0 +1,57 @@
+/*
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+top() -- Get the top element.
+getMin() -- Retrieve the minimum element in the stack.
+ 
+
+Example:
+
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   --> Returns -3.
+minStack.pop();
+minStack.top();      --> Returns 0.
+minStack.getMin();   --> Returns -2.
+*/
+class MinStack {
+
+    /** initialize your data structure here. */
+    vector<int> min;
+    vector<int> stack;
+    public:
+    MinStack() {
+        
+    }
+    
+    void push(int x) {
+        stack.push_back(x);
+        if(min.empty()||min.back()>=x)
+            min.push_back(x);
+    }
+    
+    void pop() {
+        if(stack.back()==min.back())
+            min.pop_back();
+        
+        stack.pop_back();
+    }
+    
+    int top() {
+        if(!stack.empty())
+        return stack.back();
+        
+        return -1;
+    }
+    
+    int getMin() {
+        if(!min.empty())
+        return min.back();
+        
+        return -1;
+    }
+};
