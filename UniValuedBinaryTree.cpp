7e468ef5b395e053972de9455f028e1ce90746d9
discoversaurabh <discover.saurabh@gmail.com>
2020-04-15 02:14:10 +0530

    Univalued Binary Tree (each node has the same value)
    https://leetcode.com/problems/univalued-binary-tree/

---
+++ UniValuedBinaryTree.cpp
@@ -0,0 +1,43 @@
+/*
+Univalued Binary Tree
+A binary tree is univalued if every node in the tree has the same value.
+Return true if and only if the given tree is univalued.
+
+Example 1:
+Input: [1,1,1,1,1,null,1]
+Output: true
+
+Example 2:
+Input: [2,2,2,5,2]
+Output: false
+ 
+Note:
+The number of nodes in the given tree will be in the range [1, 100].
+Each node's value will be an integer in the range [0, 99].
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    
+    bool checkUniVal(int val, TreeNode *node)
+    {
+        if(!node) return true;
+        
+        if(node->val == val)
+            return checkUniVal(val, node->left) && checkUniVal(val, node->right); 
+        else
+            return false;
+    }
+    
+    bool isUnivalTree(TreeNode* root) {
+     return checkUniVal(root->val, root->left) && checkUniVal(root->val, root->right);   
+    }
+};
