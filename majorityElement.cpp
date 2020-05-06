ce99b46faa4966fbd4bda22ea8367d2d7eade897
discoversaurabh <discover.saurabh@gmail.com>
2020-05-06 18:57:30 +0530

    Majority Element [Day 6/30 May LeetCode]
    https://leetcode.com/problems/majority-element/

---
+++ majorityElement.cpp
@@ -0,0 +1,37 @@
+/*
+Majority Element
+
+Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+You may assume that the array is non-empty and the majority element always exist in the array.
+
+Example 1:
+Input: [3,2,3]
+Output: 3
+
+Example 2:
+Input: [2,2,1,1,1,2,2]
+Output: 2
+*/
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int size = nums.size(), majNum = nums[0], majNumCount = 1;
+        for(int i=1; i<size; i++)
+        {
+            if(nums[i] != majNum)
+            {
+                majNumCount--;
+                if(majNumCount < 0)
+                {
+                    majNum = nums[i];
+                    majNumCount = 1;
+                }
+            }
+            else
+                majNumCount++;
+        }
+        return majNum;
+    }
+};
